f760335e49d0cdcca01bb1e28ab0421de90f113e
lounesbbkr <99367325+lounesbbkr@users.noreply.github.com>
2024-02-27 01:31:35 +0100

    regle

--- search_sequence.py
+++ search_sequence.py
@@ -9,6 +9,7 @@ def get_next_32_numbers(sequence):
             yield ""
 
     def find_sequence_in_file(user_sequence, filename):
+        user_sequence = ''.join(user_sequence)  # Convert list to string
         for file_sequence in read_sequence_from_file(filename):
             if not file_sequence:
                 return None
@@ -16,10 +17,7 @@ def get_next_32_numbers(sequence):
                 index = file_sequence.index(user_sequence)
                 sequence_end = index + len(user_sequence)
                 next_32_numbers = file_sequence[sequence_end:sequence_end + 32]
-                if len(user_sequence) < 32:
-                    return user_sequence + next_32_numbers[:32 - len(user_sequence)]
-                else:
-                    return next_32_numbers
+                return next_32_numbers
         return None
 
     filename = "base_pi.txt"
